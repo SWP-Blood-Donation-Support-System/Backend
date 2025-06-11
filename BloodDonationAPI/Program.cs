f923af517e3eae29d3bf4b88edcd25f70644bac9
nguyen phi khanh <khanhnpse183527@fpt.edu.vn>
2025-06-11 15:26:02 +0700

    chức năng tìm kiếm người hiến máu

--- BloodDonationAPI/Program.cs
+++ BloodDonationAPI/Program.cs
@@ -65,13 +65,10 @@ namespace BloodDonationAPI
                         []
                     }
                 });
-            });
-
-            builder.Services.AddScoped<IUserService, UserService>();
+            });            builder.Services.AddScoped<IUserService, UserService>();
             builder.Services.AddScoped<JwtService>();
             builder.Services.AddScoped<IAppointmentServiece, AppointmentServiece>();
-            builder.Services.AddScoped<IEmergencyService, EmergencyService>();
-            builder.Services.AddScoped<INotificationService, NotificationService>();
+            builder.Services.AddScoped<IDonorSearchService, DonorSearchService>();
 
             builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
                 .AddJwtBearer(options =>
