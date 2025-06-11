8ae00347c17036656758457eaa6d46aa9b6a7397
QHuy1910 <huyttqse183533@fpt.edu.vn>
2025-06-11 20:36:03 +0700

    them chuc nang cap nhat trang thai cho lich su dang ky

---
+++ BloodDonationAPI/DTO/AppointmentRegistrationDto.cs
@@ -0,0 +1,12 @@
+﻿namespace BloodDonationAPI.DTO
+{
+    public class AppointmentRegistrationDto
+    {
+        public int AppointmentHistoryId { get; set; }
+        public string? Username { get; set; }
+        public string? FullName { get; set; }
+        public string? Phone { get; set; }
+        public DateTime? AppointmentDate { get; set; }
+        public string? AppointmentStatus { get; set; }
+    }
+}
---
+++ BloodDonationAPI/Service/BloodDonationProcessService.cs
@@ -0,0 +1,46 @@
+﻿using BloodDonationAPI.DTO;
+using BloodDonationAPI.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace BloodDonationAPI.Service
+{
+    public class BloodDonationProcessService : IBloodDonationProcessService
+    {
+        private readonly BloodDonationSystemContext _context;
+
+        public BloodDonationProcessService(BloodDonationSystemContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<List<AppointmentRegistrationDto>> GetRegistrationsByAppointmentID(int AppointmentID)
+        {
+            return await _context.AppointmentHistories
+                 .Where(h => h.AppointmentId == AppointmentID)
+                 .Include(h => h.UsernameNavigation)
+                 .Select(h => new AppointmentRegistrationDto
+                 {
+                     AppointmentHistoryId = h.AppointmentHistoryId,
+                     Username = h.Username,
+                     FullName = h.UsernameNavigation.FullName,
+                     Phone = h.UsernameNavigation.Phone,
+                     AppointmentDate = h.AppointmentDate,
+                     AppointmentStatus = h.AppointmentStatus
+                 })
+                 .ToListAsync();
+
+        }
+
+        public async Task<bool> UpdateAppointmentStatusAsync(UpdateAppointmentStatusDto updateDto)
+        {
+            var history = await _context.AppointmentHistories.FirstOrDefaultAsync(h => h.AppointmentHistoryId == updateDto.AppointmentHistoryId);
+            if (history == null)
+                return false;
+
+            history.AppointmentStatus = updateDto.AppointmentStatus;
+            await _context.SaveChangesAsync();
+
+            return true;
+        }
+    }
+}
